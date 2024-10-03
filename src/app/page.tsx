6396697c27aedfc494b19520f3e26775aac625b5
Adam Kiryk <adamkiryk@gmail.com>
2024-10-03 06:45:22 -0400

    seed db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-import data from "@/assets/new-music.json";
+// import albums from "@/assets/new-music.json";
+import { getAlbums } from "@/db/index";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const albums = await getAlbums();
   return (
     <main className="mx-8 my-8">
-      <h1>New Music Albums</h1>
+      <h1>New Music Albums from DB!</h1>
       <ul>
-        {data.map((album, index) => (
+        {albums.map((album, index) => (
           <li key={index} className={styles.listing}>
             <div className={styles.details}>
               <span>Artist:</span> <span>{album.artist}</span>
             </div>
             <div className={styles.details}>
               <span>Album:</span>
-              <span>{album.album}</span>
+              <span>{album.title}</span>
             </div>
           </li>
         ))}
